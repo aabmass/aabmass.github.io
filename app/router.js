feed8adc1cdabc61f7caa9f509ce9f2c73cab689
Aaron Abbott <aabmass@gmail.com>
2016-05-24 12:06:06 -0400

    Added google analytics

--- app/router.js
+++ app/router.js
@@ -2,7 +2,26 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+
+  /* for google analytics a la ember-metrics */
+  metrics: Ember.inject.service(),
+
+  didTransition() {
+    this._super(...arguments);
+    
+    // inject analytics js into page
+    this._trackPage();
+  },
+  
+  _trackPage() {
+    Ember.run.scheduleOnce('afterRender', this, () => {
+      const page = document.location.pathname;
+      const title = this.getWithDefault('currentRouteName', 'unknown');
+
+      this.get('metrics').trackPage('GoogleAnalytics', { page, title });
+    });
+  }
 });
 
 Router.map(function() {
