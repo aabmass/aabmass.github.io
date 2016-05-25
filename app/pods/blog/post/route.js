abbb427f3fe84c9191e64f06956e80e4927c2fed
Aaron Abbott <aabmass@gmail.com>
2016-05-25 14:08:27 -0400

    404 on blog post json redirects to page-not-found

--- app/pods/blog/post/route.js
+++ app/pods/blog/post/route.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('blog-post', params.id);
+  },
+  actions: {
+    error(reason, transition) {
+      const error = reason.errors[0];
+
+      // redirect to page-not-found route for a 404
+      if (error.status == 404) {
+        // slice off the leading '/'
+        var pathToWrite = transition.intent.url.slice(1);
+
+        this.transitionTo('page-not-found', pathToWrite);
+      }
+    }
   }
 });
