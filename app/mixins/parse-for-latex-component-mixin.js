880b13cb45282f9946f4b2d57183d10b88668219
Aaron Abbott <aabmass@gmail.com>
2016-05-26 14:59:12 -0400

    katex to use $ ... $ for inline math

--- app/mixins/parse-for-latex-component-mixin.js
+++ app/mixins/parse-for-latex-component-mixin.js
@@ -4,6 +4,28 @@ export default Ember.Mixin.create({
   /** This is a mixin to be mixed with a component! */
 
   didUpdate() {
-    renderMathInElement(this.get('element'));
+    renderMathInElement(this.get('element'), {
+      delimiters: [
+        {
+          left: "$$",
+          right: "$$",
+          display: true
+        },
+        {
+          left: "\\[", right: "\\]",
+          display: true
+        },
+        {
+          left: "\\(", right: "\\)",
+          display: false
+        },
+        /* also parse $ ... $ as inline latex */
+        {
+          left: "$",
+          right: "$",
+          display: false
+        }
+      ]
+    });
   }
 });
