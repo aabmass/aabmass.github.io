53d4199480654aabe281b2c03eb9f9ead15bb51d
Aaron Abbott <aabmass@gmail.com>
2016-05-26 11:53:59 -0400

    Added a script to easy deployment

---
+++ deploy-ghpages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cd to the directory of this script
+cd "$(dirname "$0")"
+
+rm -rf dist/ tmp/
+ember build -prod
+cd pub-branch
+git checkout master
+rm -rf *
+cp -R ../dist/* .
+git add -A
+
+# starts with the -m message than opens the editor
+git commit -m "Deployed on `date`" -e
+git push
